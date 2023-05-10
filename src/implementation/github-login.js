372b4e04c108c7f7459ef501861affa27c6237a5
Erki Aas <eaas@k-space.ee>
2023-05-10 18:31:58 +0300

    Implement getting organizations and teams(groups) from GitHub

--- src/implementation/github-login.js
+++ src/implementation/github-login.js
@@ -3,6 +3,7 @@ import Account from "../support/account.js";
 import {OAuth2} from "oauth";
 import accessDenied from "../support/access-denied.js";
 import getLoginResult from "../support/get-login-result.js";
+import {GitHubGroupPrefix} from "../support/kube-constants.js";
 
 export default async (ctx, provider) => {
     const ghOauth = new OAuth2(process.env.GH_CLIENT_ID,
@@ -23,7 +24,7 @@ export default async (ctx, provider) => {
         ctx.status = 302;
         return ctx.redirect(ghOauth.getAuthorizeUrl({
             redirect_uri: `${process.env.ISSUER_URL}interaction/callback/gh`,
-            scope: ['user:email'],
+            scope: ['user:email,read:org'],
             state,
         }));
     }
@@ -63,13 +64,74 @@ export default async (ctx, provider) => {
         id: user.id,
         login: user.login,
     }
-    // TODO: githubGroups
+
+    let githubGroups = []
+    try {
+        githubGroups = await getUserOrganizations(token).then(organizations => {
+            return Promise.all(organizations.map(organization => {
+                return getOrganizationTeams(token, organization).then(teams => {
+                    return Promise.all(teams.map(team => {
+                        return getUserOrganizationTeamMembership(token, team, user.login)
+                    }))
+                })
+            })).then(g => {
+                g = g.flat(2).filter(r => !!r)
+                g = [
+                    ...g,
+                    ...organizations
+                ]
+                return g
+            })
+        })
+        githubGroups = githubGroups.map(g => {
+            return {
+                prefix: GitHubGroupPrefix,
+                name: g,
+            }
+        })
+    } catch (e) {
+        console.error('Error getting groups from GitHub: ' + e)
+    }
+
     await ctx.kubeApiService.updateUserSpec({
         accountId: account.accountId,
-        githubProfile
+        githubProfile,
+        githubGroups
     })
 
     return provider.interactionFinished(ctx.req, ctx.res, await getLoginResult(ctx, provider, account), {
         mergeWithLastSubmission: false,
     });
+}
+
+const getUserOrganizations = async (token) => {
+    return await fetch(`https://api.github.com/user/memberships/orgs`, {
+        method: "GET",
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+    }).then(r => r.json()).then(r => r.filter(r => r.state === 'active')).then(r => r.map(r => r?.organization?.login.toLowerCase()))
+}
+
+const getOrganizationTeams = async (token, org) => {
+    return await fetch( `https://api.github.com/orgs/${org}/teams`, {
+        method: "GET",
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+    }).then((r) => r.json()).then(r => r.map(r => {
+        return {
+            ...r,
+            organization: org
+        }
+    }));
+}
+
+const getUserOrganizationTeamMembership = async (token, team, userLogin) => {
+    return await fetch(`https://api.github.com/orgs/${team.organization}/teams/${team.slug}/memberships/${userLogin}`, {
+        method: "GET",
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+    }).then((r) => r.json()).then(r => r?.state === 'active' ? `${team.organization}:${team.slug}` : null);
 }
