bbcd0011f9fdf11ea0ac76c9b72331de64bc2d95
Erki Aas <eaas@k-space.ee>
2023-05-23 15:30:07 +0300

    Make approvalRequiredPolicy first

--- src/implementation/setup-policies.js
+++ src/implementation/setup-policies.js
@@ -4,26 +4,7 @@ import {Approved} from "../support/conditions/approved.js";
 
 export default () => {
     const { Prompt, Check, base } = interactionPolicy;
-
-    const tosPolicy = new Prompt(
-        { name: 'tos', requestable: true },
-        new Check('tos_not_accepted', 'ToS needs to be accepted', 'interaction_required', async (ctx) => {
-                const { oidc, kubeOIDCUserService } = ctx;
-                const kubeUser = await kubeOIDCUserService.findUser(oidc.session.accountId)
-                return !kubeUser.checkCondition(new ToSv1())
-            },
-        ),
-    )
-
-    const namePolicy = new Prompt(
-        { name: 'name', requestable: true },
-        new Check('name_required', 'User profile requires name', 'interaction_required', async (ctx) => {
-                const { oidc, kubeOIDCUserService } = ctx;
-                const kubeUser = await kubeOIDCUserService.findUser(oidc.session.accountId)
-                return kubeUser.profile.name ? Check.NO_NEED_TO_PROMPT : Check.REQUEST_PROMPT;
-            },
-        ),
-    )
+    const basePolicy = base()
 
     const approvalRequiredPolicy = new Prompt(
         { name: 'approval_required', requestable: false },
@@ -34,10 +15,29 @@ export default () => {
             },
         ),
     )
-
-    const basePolicy = base()
-    basePolicy.add(tosPolicy)
-    basePolicy.add(namePolicy)
     basePolicy.add(approvalRequiredPolicy)
+
+    const tosPolicy = new Prompt(
+        { name: 'tos', requestable: true },
+        new Check('tos_not_accepted', 'ToS needs to be accepted', 'interaction_required', async (ctx) => {
+                const { oidc, kubeOIDCUserService } = ctx;
+                const kubeUser = await kubeOIDCUserService.findUser(oidc.session.accountId)
+                return !kubeUser.checkCondition(new ToSv1())
+            },
+        ),
+    )
+    basePolicy.add(tosPolicy)
+
+    const namePolicy = new Prompt(
+        { name: 'name', requestable: true },
+        new Check('name_required', 'User profile requires name', 'interaction_required', async (ctx) => {
+                const { oidc, kubeOIDCUserService } = ctx;
+                const kubeUser = await kubeOIDCUserService.findUser(oidc.session.accountId)
+                return kubeUser.profile.name ? Check.NO_NEED_TO_PROMPT : Check.REQUEST_PROMPT;
+            },
+        ),
+    )
+    basePolicy.add(namePolicy)
+
     return basePolicy
 }
