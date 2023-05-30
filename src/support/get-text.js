27211b09068f68a52b45fc4fe34a3ccb51632ebf
Erki Aas <eaas@k-space.ee>
2023-05-30 03:03:12 +0300

    Add placeholder indicating need for configuration to getText function

--- src/support/get-text.js
+++ src/support/get-text.js
@@ -5,7 +5,7 @@ export const ApprovalTextName = 'approval'
 export const ToSTextName = 'tos'
 
 export function getText(name) {
-    let text = ''
+    let text = `Please add /app/${name}/${name}.txt using ConfigMap.`
     if (existsSync(`/app/${name}/${name}.txt`)) {
         text = readFileSync(`/app/${name}/${name}.txt`, 'utf8');
     }
