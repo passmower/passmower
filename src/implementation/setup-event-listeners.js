6e57d9102e22911a6cea8b95a42fd63ea25bfa87
Erki Aas <erki@maagiline.ee>
2023-06-11 17:25:34 +0300

    Log provider events on debug level

--- src/implementation/setup-event-listeners.js
+++ src/implementation/setup-event-listeners.js
@@ -5,218 +5,218 @@ export default (provider) => {
 
     // Logging
     provider.on('access_token.destroyed', (token) => {
-        logger.info({}, 'access_token.destroyed')
+        logger.debug({}, 'access_token.destroyed')
     })
 
     provider.on('access_token.saved', (token) => {
-        logger.info({}, 'access_token.saved')
+        logger.debug({}, 'access_token.saved')
     })
 
     provider.on('access_token.issued', (token) => {
-        logger.info({}, 'access_token.issued')
+        logger.debug({}, 'access_token.issued')
     })
 
     provider.on('authorization_code.consumed', (code) => {
-        logger.info({}, 'authorization_code.consumed')
+        logger.debug({}, 'authorization_code.consumed')
     })
 
     provider.on('authorization_code.destroyed', (code) => {
-        logger.info({}, 'authorization_code.destroyed')
+        logger.debug({}, 'authorization_code.destroyed')
     })
 
     provider.on('authorization_code.destroyed', (code) => {
-        logger.info({}, 'authorization_code.destroyed')
+        logger.debug({}, 'authorization_code.destroyed')
     })
 
     provider.on('authorization.accepted', (ctx) => {
-        logger.info({ctx}, 'authorization.accepted')
+        logger.debug({ctx}, 'authorization.accepted')
     })
 
     provider.on('authorization.error', (ctx, error) => {
-        logger.info({ctx, error}, 'authorization.error')
+        logger.debug({ctx, error}, 'authorization.error')
     })
 
     provider.on('authorization.success', (ctx) => {
-        logger.info({ctx}, 'authorization.success')
+        logger.debug({ctx}, 'authorization.success')
     })
 
     provider.on('backchannel.error', (ctx, error, client, accountId, sid) => {
-        logger.info({ctx, error, client, accountId, sid}, 'backchannel.error')
+        logger.debug({ctx, error, client, accountId, sid}, 'backchannel.error')
     })
 
     provider.on('backchannel.success', (ctx, client, accountId, sid) => {
-        logger.info({ctx, client, accountId, sid}, 'backchannel.success')
+        logger.debug({ctx, client, accountId, sid}, 'backchannel.success')
     })
 
     provider.on('jwks.error', (ctx, error) => {
-        logger.info({ctx, error}, 'jwks.error')
+        logger.debug({ctx, error}, 'jwks.error')
     })
 
     provider.on('client_credentials.destroyed', (token) => {
-        logger.info({}, 'client_credentials.destroyed')
+        logger.debug({}, 'client_credentials.destroyed')
     })
 
     provider.on('client_credentials.saved', (token) => {
-        logger.info({}, 'client_credentials.saved')
+        logger.debug({}, 'client_credentials.saved')
     })
 
     provider.on('client_credentials.issued', (token) => {
-        logger.info({}, 'client_credentials.issued')
+        logger.debug({}, 'client_credentials.issued')
     })
 
     provider.on('device_code.consumed', (code) => {
-        logger.info({}, 'device_code.consumed')
+        logger.debug({}, 'device_code.consumed')
     })
 
     provider.on('device_code.destroyed', (code) => {
-        logger.info({}, 'device_code.destroyed')
+        logger.debug({}, 'device_code.destroyed')
     })
 
     provider.on('device_code.saved', (code) => {
-        logger.info({}, 'device_code.saved')
+        logger.debug({}, 'device_code.saved')
     })
 
     provider.on('discovery.error', (ctx, error) => {
-        logger.info({ctx, error}, 'discovery.error')
+        logger.debug({ctx, error}, 'discovery.error')
     })
 
     provider.on('end_session.error', (ctx, error) => {
-        logger.info({ctx, error}, 'end_session.error')
+        logger.debug({ctx, error}, 'end_session.error')
     })
 
     provider.on('end_session.success', (ctx) => {
-        logger.info({ctx}, 'end_session.success')
+        logger.debug({ctx}, 'end_session.success')
     })
 
     provider.on('grant.error', (ctx, error) => {
-        logger.info({ctx, error}, 'grant.error')
+        logger.debug({ctx, error}, 'grant.error')
     })
 
     provider.on('grant.revoked', (ctx, grantId) => {
-        logger.info({ctx, grantId}, 'grant.revoked')
+        logger.debug({ctx, grantId}, 'grant.revoked')
     })
 
     provider.on('grant.success', (ctx) => {
-        logger.info({ctx}, 'grant.success')
+        logger.debug({ctx}, 'grant.success')
     })
 
     provider.on('initial_access_token.destroyed', (token) => {
-        logger.info({}, 'initial_access_token.destroyed')
+        logger.debug({}, 'initial_access_token.destroyed')
     })
 
     provider.on('initial_access_token.saved', (token) => {
-        logger.info({}, 'initial_access_token.saved')
+        logger.debug({}, 'initial_access_token.saved')
     })
 
     provider.on('interaction.destroyed', (interaction) => {
-        logger.info({interaction}, 'interaction.destroyed')
+        logger.debug({interaction}, 'interaction.destroyed')
     })
 
     provider.on('interaction.ended', (ctx) => {
-        logger.info({ctx}, 'interaction.ended')
+        logger.debug({ctx}, 'interaction.ended')
     })
 
     provider.on('interaction.saved', (interaction) => {
-        logger.info({interaction}, 'interaction.saved')
+        logger.debug({interaction}, 'interaction.saved')
     })
 
     provider.on('interaction.started', (ctx, prompt) => {
-        logger.info({ctx, prompt}, 'interaction.started')
+        logger.debug({ctx, prompt}, 'interaction.started')
     })
 
     provider.on('introspection.error', (ctx, error) => {
-        logger.info({ctx, error}, 'interaction.destroyed')
+        logger.debug({ctx, error}, 'interaction.destroyed')
     })
 
     provider.on('replay_detection.destroyed', (token) => {
-        logger.info({}, 'replay_detection.destroyed')
+        logger.debug({}, 'replay_detection.destroyed')
     })
 
     provider.on('replay_detection.saved', (token) => {
-        logger.info({}, 'replay_detection.saved')
+        logger.debug({}, 'replay_detection.saved')
     })
 
     provider.on('pushed_authorization_request.error', (ctx, error) => {
-        logger.info({ctx, error}, 'pushed_authorization_request.error')
+        logger.debug({ctx, error}, 'pushed_authorization_request.error')
     })
 
     provider.on('pushed_authorization_request.success', (ctx, client) => {
-        logger.info({ctx, client}, 'pushed_authorization_request.success')
+        logger.debug({ctx, client}, 'pushed_authorization_request.success')
     })
 
     provider.on('pushed_authorization_request.destroyed', (token) => {
-        logger.info({}, 'pushed_authorization_request.destroyed')
+        logger.debug({}, 'pushed_authorization_request.destroyed')
     })
 
     provider.on('pushed_authorization_request.saved', (token) => {
-        logger.info({}, 'pushed_authorization_request.saved')
+        logger.debug({}, 'pushed_authorization_request.saved')
     })
 
     provider.on('refresh_token.consumed', (token) => {
-        logger.info({}, 'refresh_token.consumed')
+        logger.debug({}, 'refresh_token.consumed')
     })
 
     provider.on('refresh_token.destroyed', (token) => {
-        logger.info({}, 'refresh_token.destroyed')
+        logger.debug({}, 'refresh_token.destroyed')
     })
 
     provider.on('refresh_token.saved', (token) => {
-        logger.info({}, 'refresh_token.saved')
+        logger.debug({}, 'refresh_token.saved')
     })
 
     provider.on('registration_access_token.destroyed', (token) => {
-        logger.info({}, 'registration_access_token.destroyed')
+        logger.debug({}, 'registration_access_token.destroyed')
     })
 
     provider.on('registration_access_token.saved', (token) => {
-        logger.info({}, 'registration_access_token.saved')
+        logger.debug({}, 'registration_access_token.saved')
     })
 
     provider.on('registration_create.error', (ctx, error) => {
-        logger.info({ctx, error}, 'registration_create.error')
+        logger.debug({ctx, error}, 'registration_create.error')
     })
 
     provider.on('registration_create.success', (ctx, client) => {
-        logger.info({ctx, client}, 'registration_create.success')
+        logger.debug({ctx, client}, 'registration_create.success')
     })
 
     provider.on('registration_delete.error', (ctx, error) => {
-        logger.info({ctx, error}, 'registration_delete.error')
+        logger.debug({ctx, error}, 'registration_delete.error')
     })
 
     provider.on('registration_delete.success', (ctx, client) => {
-        logger.info({ctx, client}, 'registration_delete.success')
+        logger.debug({ctx, client}, 'registration_delete.success')
     })
 
     provider.on('registration_read.error', (ctx, error) => {
-        logger.info({ctx, error}, 'registration_read.error')
+        logger.debug({ctx, error}, 'registration_read.error')
     })
 
     provider.on('registration_update.error', (ctx, error) => {
-        logger.info({ctx, error}, 'registration_update.error')
+        logger.debug({ctx, error}, 'registration_update.error')
     })
 
     provider.on('registration_update.success', (ctx, client) => {
-        logger.info({ctx, client}, 'registration_update.success')
+        logger.debug({ctx, client}, 'registration_update.success')
     })
 
     provider.on('revocation.error', (ctx, error) => {
-        logger.info({ctx, error}, 'revocation.error')
+        logger.debug({ctx, error}, 'revocation.error')
     })
 
     provider.on('server_error', (ctx, error) => {
-        logger.info({ctx, error}, 'server_error')
+        logger.debug({ctx, error}, 'server_error')
     })
 
     provider.on('session.destroyed', (session) => {
-        logger.info({}, 'session.destroyed')
+        logger.debug({}, 'session.destroyed')
     })
 
     provider.on('session.saved', (session) => {
-        logger.info({}, 'session.saved')
+        logger.debug({}, 'session.saved')
     })
 
     provider.on('userinfo.error', (ctx, error) => {
-        logger.info({ctx, error}, 'userinfo.error')
+        logger.debug({ctx, error}, 'userinfo.error')
     })
 }
