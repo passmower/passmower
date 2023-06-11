0b51ab64bb576a904b15f7cdc9f9ade19f3efd74
Erki Aas <erki@maagiline.ee>
2023-06-11 17:24:40 +0300

    Use global logger on debug level for caught OIDCProvider errors

--- src/support/render-error.js
+++ src/support/render-error.js
@@ -1,7 +1,7 @@
 import htmlSafe from "oidc-provider/lib/helpers/html_safe.js";
 
 const renderError = (ctx, out, error) => {
-    console.error(error)
+    globalThis.logger.debug({ctx, out, error})
     ctx.type = 'html';
     ctx.body = `<!DOCTYPE html>
     <head>
